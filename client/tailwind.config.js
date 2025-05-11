83dd06319ca0284df87c0a1eafec713b65529fef
leowu0329 <ryowuandjanet@gmail.com>
2025-05-12 00:50:18 +0800

    complete dark mode functionality

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,14 @@ export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [import('flowbite/plugin')],
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin'),
+  ],
+  darkMode: 'class',
 };
